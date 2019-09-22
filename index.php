e407e7dd46d3ce5b613eba0c4e88453abe11ba3b
evaldasdoda <55666891+evaldasdoda@users.noreply.github.com>
2019-09-22 21:46:57 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Project Q is new way to use quizes to improve your sales">
+    <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    <link rel="stylesheet" type="text/css" href="resources/css/queries.css">
+    
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400,400i" rel="stylesheet">
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="resources/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="resources/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="resources/favicons/favicon-16x16.png">
+    <link rel="manifest" href="resources/favicons/site.webmanifest">
+    <link rel="mask-icon" href="resources/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="resources/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="resources/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <title>Quiz</title>
+</head>
+   <body>
+    <header>
+       <nav>
+           <div class="row">
+               <a href="index.html" ><img src="resources/img/logo.png" alt="Quizes logo" class="logo"></a>
+               <a href="index.html" ><img src="resources/img/logo-black.png" alt="Quizes logo" class="logo-black"></a>
+               <ul class="main-nav js--main-nav">
+                   <li><a href="index.php#why-usid">Why us </a></li>
+                   <li><a href="index.php#how-it-workId">How it works </a></li>
+                   <li><a href="index.php#companiesId">Companies </a></li>
+                   <li><a href="index.php#pricingId">Pricing</a></li>
+                   <li><a href="register.php">Sign up </a></li>
+                   <li><a href="login.php">Log in</a></li>
+                   
+               </ul>
+               
+               <a class="mobile-nav-icon js--nav-icon"> <i class="ion-navicon-round"></i></a>
+           </div>
+       </nav>
+        <div class="hero-text-box">
+            <h1>Start your experience <br> New way to use Quizes</h1>
+            <a class="btn btn-full js--scroll-to-plan" href="#">Premium account</a>
+            <a class="btn btn-ghost btn-header" href="register.html">Free account</a>
+        </div>
+    </header>
+    <section class="section-features js--section-features" id="why-usid">
+        <div class="row" >
+           
+            <h2>New way &mdash; better experience.</h2>
+            <p class="long-copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida enim enim, at molestie elit sagittis ac. Vivamus vehicula quam elit, at fringilla arcu maximus nec.</p>
+        </div>
+        <div class="row js--wp-1">
+           <!-- col klasa z grid.css, dodajim klasa box zeb css zrobic, nigdy nie mieniami col span z grida!!!!! -->
+		   
+           <div class="col span-1-of-4 box">
+               <i class="ion-ios-color-wand-outline icon-big"></i>
+                <h3>New experience</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida enim enim, at molestie elit sagittis ac. Vivamus vehicula quam elit, at fringilla arcu maximus nec.</p>
+            </div>
+            <div class="col span-1-of-4 box">
+               <i class="ion-ios-pricetag-outline icon-big"></i>
+                <h3>Good pricing</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida enim enim, at molestie elit sagittis ac. Vivamus vehicula quam elit, at fringilla arcu maximus nec.</p>
+            </div>
+            <div class="col span-1-of-4 box">
+               <i class="ion-ios-chatboxes-outline icon-big"></i>
+                <h3>24/7 support</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida enim enim, at molestie elit sagittis ac. Vivamus vehicula quam elit, at fringilla arcu maximus nec.</p>
+            </div>
+            <div class="col span-1-of-4 box">
+               <i class="ion-ios-cart-outline icon-big"></i>
+                <h3>Full refund</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida enim enim, at molestie elit sagittis ac. Vivamus vehicula quam elit, at fringilla arcu maximus nec.</p>
+            </div>
+            
+        </div>
+    </section>
+    
+    
+    <section class="section-steps">
+       <div class="row" id="how-it-workId">
+           <h2>How it works - Simple as 1, 2, 3</h2>
+       </div>
+       <div class="row">
+           <div class="col span-1-of-2 steps-box">
+               <img src="resources/img/question.svg" alt="questions" class="step-img-question js--wp-2">
+           </div>
+           <div class="col span-1-of-2 steps-box">
+               <div class="works-step">
+                    <div>1</div>
+                    <p>Choose the subscription plan that best fits your needs and sign up today.</p>
+               </div>
+                <div class="works-step">
+                    <div>2</div>
+                    <p>Make <br> quiz.</p>
+               </div>
+                <div class="works-step">
+                    <div>3</div>
+                    <p>Copy link <br> and send to someone.</p>
+               </div>
+           </div>
+       </div>
+        
+    </section>
+        
+    <section class="section-cities js--wp-3">
+        <div class="row" id="companiesId">
+           <h2>Companies which use us</h2>
+       </div>
+       <div class = "row">
+           <div class="col span-1-of-4 box">
+               <img src="resources/img/lisbon-3.jpg" alt="Lisbon">
+               <h3>Company name </h3>
+                <div class="company-feature">
+                    <i class="ion-ios-redo-outline icon-small"></i>
+                    <a href="#">@link_here</a>
+                </div>
+           </div>
+           
+           <div class="col span-1-of-4 box">
+               <img src="resources/img/san-francisco.jpg" alt="San-francisco">
+               <h3>Company name</h3>
+                <div class="company-feature">
+                    <i class="ion-ios-redo-outline icon-small"></i>
+                    <a href="#">@link_here</a>
+                </div>
+           </div>
+           
+           <div class="col span-1-of-4 box">
+               <img src="resources/img/berlin.jpg" alt="San-francisco">
+               <h3>Company name</h3>
+                <div class="company-feature">
+                    <i class="ion-ios-redo-outline icon-small"></i>
+                    <a href="#">@link_here</a>
+                </div>
+           </div>
+           
+           <div class="col span-1-of-4 box">
+               <img src="resources/img/london.jpg" alt="San-francisco">
+               <h3>Company name</h3>
+                <div class="company-feature">
+                    <i class="ion-ios-redo-outline icon-small"></i>
+                    <a href="#">@link_here</a>
+                </div>
+           </div>
+           
+       </div>
+       
+    </section>
+    
+    <section class="section-testimonials">
+        <div class="row"> 
+            <h2>Reviews</h2>
+        </div>
+        <div class="row">
+            <div class="col span-1-of-3">
+                <blockquote>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida enim enim, at molestie elit sagittis ac. Vivamus vehicula quam elit, at fringilla arcu maximus nec.
+                    <cite><img src="resources/img/customer-1.jpg" alt="Customer 1">Alberto Duncan</cite>
+                </blockquote>
+            </div>
+            <div class="col span-1-of-3">
+                <blockquote>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida enim enim, at molestie elit sagittis ac. Vivamus vehicula quam elit, at fringilla arcu maximus nec.
+                    <cite><img src="resources/img/customer-2.jpg" alt="Customer 1">Joana Silva</cite>
+                </blockquote>
+            </div>
+            <div class="col span-1-of-3">
+                <blockquote>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida enim enim, at molestie elit sagittis ac. Vivamus vehicula quam elit, at fringilla arcu maximus nec.
+                    <cite><img src="resources/img/customer-3.jpg" alt="Customer 1">Milton Chapman</cite>
+                </blockquote>
+            </div>
+        </div>
+    </section>
+    
+    <section class="section-plans js--section-plans">
+       <div class="row" id="pricingId">
+           <h2>Start your new experience today</h2>
+       </div>
+        <div class="row">
+            <div class="col span-1-of-3">
+                <div class="plan-box js--wp-4">
+                    <div>
+                        <h3>Premium</h3>
+                        <p class="plan-price">50€ </p>
+                        <p class="plan-price-meal">&nbsp;</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Lorem ipsum</li>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Support 24/7</li>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Lorem ipsum</li>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Lorem ipsum</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <a href="register.html" class="btn btn-full">Soon</a>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="col span-1-of-3">
+                <div class="plan-box">
+                    <div>
+                        <h3>Pro</h3>
+                        <p class="plan-price">30€ </p>
+                        <p class="plan-price-meal">&nbsp;</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Lorem ipsum</li>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Support 24/7</li>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Lorem ipsum</li>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Lorem ipsum</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <a href="register.html" class="btn btn-ghost">Soon</a>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="col span-1-of-3">
+                <div class="plan-box">
+                    <div>
+                        <h3>Starter</h3>
+                        <p class="plan-price">Free </p>
+                        <p class="plan-price-meal">&nbsp;</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Lorem ipsum</li>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Support from 8 am to 12 pm</li>
+                            <li><i class="ion-ios-close-empty icon-small"></i> </li>
+                            <li><i class="ion-ios-checkmark-empty icon-small"></i>Lorem ipsum</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <a href="register.php" class="btn btn-ghost">Sign up now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="section-form">
+        <div class="row">
+            <h2>We're happy to hear from you</h2>
+        </div>
+        <div class="row">
+            <form method="post" action="#" class="contact-form">
+                <div class = "row">
+                    <div class="col span-1-of-3">
+                       <!-- FOR- wpisujemy id z inputu zeb zlaczyc ich -->
+                        <label for="name">Name</label>
+                    </div>
+                    <div class="col span-2-of-3">
+                        <input type="text" name="name" id="name" placeholder="Your name" required> 
+                    </div>
+                </div>
+                
+                
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <label for="email">Email</label>
+                    </div>
+                    <div class="col span-2-of-3">
+                        <input type="email" name="email" id="email" placeholder="Your email" required> 
+                    </div>
+                </div>
+                
+                
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <label for="find-us">How did you find us?</label>
+                    </div>
+                    <div class="col span-2-of-3">
+                    <select name="find-us" id="find-us">
+                        <option value="friends" selected>Friends</option>
+                        <option value="search">Search engine</option>
+                        <option value="ad">Advertisement</option>
+                        <option value="other">Other</option>
+                    </select>
+                    </div>
+                </div>
+                
+                
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <label>Newsletter?</label>
+                    </div>
+                    <div class="col span-2-of-3">
+                        <input type="checkbox" name="news" id="news" checked> Yes, please
+                    </div>
+                </div>
+                
+                
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <label>Drop us a line?</label>
+                    </div>
+                    <div class="col span-2-of-3">
+                        <textarea name="message" placeholder="Your message" ></textarea>
+                    </div>
+                </div>
+                
+                
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <label>&nbsp;</label>
+                    </div>
+                    <div class="col span-2-of-3">
+                        <input type="submit" value="Send it">
+                    </div>
+                </div>
+            </form>
+        </div>
+        
+    </section>
+    
+    <footer>
+        <div class="row">
+            <div class="col span-1-of-2">
+                <ul class="footer-nav">
+                    <li> <a href="#">Why us</a> </li>
+                    <li> <a href="#">How it works</a> </li>
+                    <li> <a href="#">Companies</a> </li>
+                    <li> <a href="#">Pricing</a> </li>
+                    <li> <a href="#">Sign up </a> </li>
+                    
+                    <li> <a href="#">Log in </a> </li>
+                    
+                   
+                </ul>
+            </div>
+            <div class="col span-1-of-2">
+                <ul class="social-links">
+                    <li> <a href="#"> <i class="ion-social-facebook"></i> </a></li>
+                    <li> <a href="#"> <i class="ion-social-twitter"></i></a> </li>
+                    <li> <a href="#"> <i class="ion-social-googleplus"></i> </a> </li>
+                    <li> <a href="#"> <i class="ion-social-instagram"></i> </a> </li>
+                </ul>
+            </div>
+            <div class="row">
+                <p>Copyright © Project Q 2019 </p>
+            </div>
+            
+        </div>
+        
+    </footer>
+
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>JQUERRY IMPORT
+    <script src="//cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script> Pomaga dla starych browserow kozystac z respond queries
+    <script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script> Pomaga dla starych browserow kozystac z HTML5
+    <script src="//cdn.jsdelivr.net/selectivizr/1.0.3b/selectivizr.min.js"></script> Pomaga dla starych browserow uzywac CSS3
+    <script src="vendors/js/jquery.waypoints.min.js"></script>
+    <script src="resources/js/script.js"></script>-->
+    
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="//cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdn.jsdelivr.net/selectivizr/1.0.3b/selectivizr.min.js"></script>
+    <script src="vendors/js/jquery.waypoints.min.js"></script>
+    <script src="resources/js/script.js"></script>
+    
+</body>
+</html>
